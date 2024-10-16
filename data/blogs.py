eb9331c2710321ead6ca89cef1a44f39cdee9895
NadiaSbaa <nadiasbaa26@gmail.com>
2024-10-16 15:52:31 +0200

    add blogs under data

---
+++ data/blogs.py
@@ -0,0 +1,16 @@
+blogs = [
+    {"id": 1, "name": "Schutz der Haut im Winter: Risiken, Behandlungen und die Rolle von Kollagenpräparaten", "content":""},
+    {"id": 2, "name": "Veränderungen im Haar nach dem Sommer: Wie man sich darum kümmert", "content":""},
+    {"id": 3, "name": "Zwei natürliche Ergänzungsmittel zum Schutz vor der Sonne im Sommer", "content":""},
+    {"id": 4, "name": "Warum ist es wichtig, überschüssige Flüssigkeit zu minimieren, um Cellulite zu bekämpfen?", "content":""},
+    {"id": 5, "name": "Entdecke ElaBeauté mit Kollagen, um der Hautalterung vorzubeugen", "content":""},
+    {"id": 6, "name": "Stillen und Haarausfall: Warum das so ist und wie man darauf reagiert", "content":""},
+    {"id": 7, "name": "ElaBeauté: Deine natürliche Kollagenergänzung für natürliche Schönheit", "content":""},
+    {"id": 8, "name": "Operation Big Hair: Der ultimative Leitfaden für die Pflege deiner Haare in diesem Sommer", "content":""},
+    {"id": 9, "name": "Wie man natürliches Kollagen nutzt und es stimuliert", "content":""},
+    {"id": 10, "name": "Kollagenergänzung: Wofür wird sie verwendet?", "content":""},
+    {"id": 11, "name": "Haarausfall: Wann du dir Sorgen machen musst und was dagegen hilft", "content":""},
+    {"id": 12, "name": "Haarausfall nach Covid-19: Fälle nehmen zu, warum kommt es vor?", "content":""},
+    {"id": 13, "name": "Märchenhaftes HairFood!", "content":""},
+
+]
